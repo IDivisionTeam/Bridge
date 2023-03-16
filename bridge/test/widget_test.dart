349e5871a2d4ff6b3d52d397b7f8d96a3268f16f
yaroslav-android <yaroslav.ovdiienko@uptech.team>
2023-03-16 13:26:04 +0200

    test: fix blank test

--- bridge/test/widget_test.dart
+++ bridge/test/widget_test.dart
@@ -4,5 +4,6 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   testWidgets('blank test', (WidgetTester tester) async {
     await tester.pumpWidget(const Application());
+    await tester.pumpAndSettle(const Duration(seconds: 1));
   });
 }
